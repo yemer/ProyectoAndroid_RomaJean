579eb3646aeb4e18b290440ff9a4f6a515f41766
unknown <Admin@IHR80PBF371.continental.edu.pe>
2019-12-14 12:00:45 -0500

    login

---
+++ app/src/main/java/com/example/romajeansof/ProductosParaVarones.java
@@ -0,0 +1,14 @@
+package com.example.romajeansof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductosParaVarones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos_para_varones);
+    }
+}
